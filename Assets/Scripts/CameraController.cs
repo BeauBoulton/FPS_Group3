0703343f055378d0c1c40ec609a423d66864eede
BeauBoulton <beauboulton@gmail.com>
2025-04-22 17:42:38 -0700

    Added Player

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+public class CameraController : MonoBehaviour
+{
+    Vector2 mouseLook;
+    Vector2 smoothV;
+    public float sensitivity = 5.0f;
+    public float smoothing = 2.0f;
+
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = this.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mouseDirection = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        mouseDirection = Vector2.Scale(mouseDirection, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothV.x = Mathf.Lerp(smoothV.x, mouseDirection.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, mouseDirection.y, 1f / smoothing);
+        mouseLook += smoothV;
+
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -90f, 90f);
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        player.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, player.transform.up);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody rigidBody;
+    public int speed;
+    public GameObject bullet; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnProjectile();
+        }
         
+        float translation = Input.GetAxis("Vertical") * 10 * Time.deltaTime;
+        float straffe = Input.GetAxis("Horizontal") * 10 * Time.deltaTime;
+
+        transform.Translate(straffe, 0, translation);
+    }
+
+    public void SpawnProjectile()
+    {
+        GameObject projectile = Instantiate(bullet, transform.position, bullet.transform.rotation);
     }
 }
