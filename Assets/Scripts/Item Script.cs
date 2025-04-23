666dd61993f02822b86df6c5132cc55ff51a5f2d
OnePunchWeeb <nsumek@gmail.com>
2025-04-22 17:41:24 -0700

    declares the pick ups
    made an item script

---
+++ Assets/Scripts/Item Script.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * Nick Sumek
+ * 4/22/25
+ * Sets up basic values for the items
+ */
+public class ItemScript : MonoBehaviour
+{
+    //declares item pick ups
+    public int healthPack;
+    public bool hasKeyCard;
+    public int movementBoost;
+    public int doubleDamage;
+    public bool isInvincible;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
