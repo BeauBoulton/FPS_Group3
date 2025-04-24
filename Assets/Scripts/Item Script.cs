d577f04abf6108c1b58340b3a8ba494eb32a678b
OnePunchWeeb <nsumek@gmail.com>
2025-04-24 16:54:56 -0700

    Sets up some pick ups
    Invuln and health should be working now

--- Assets/Scripts/Item Script.cs
+++ Assets/Scripts/Item Script.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 /*
  * Nick Sumek
- * 4/22/25
+ * updated 4/24/25
  * Sets up basic values for the items
  */
 public class ItemScript : MonoBehaviour
@@ -13,7 +14,67 @@ public class ItemScript : MonoBehaviour
     public bool hasKeyCard;
     public int movementBoost;
     public int doubleDamage;
-    public bool isInvincible;
+    public int playerHealth;
+
+    // Variables for iframes
+    private bool isInvincible = false;
+    public int iFramesTime = 5;
+
+    //sets up item pick ups
+    private void OnTriggerEnter(Collider other)
+    {
+        //Health Pick Ups
+            {
+            if (other.gameObject.tag == "Health")
+                {
+                 playerHealth = 100;
+                }
+            }
+
+        //sets up double damage
+        if (other.gameObject.tag == "Damage Buff")
+        {
+           //get the projectile damage and multiply by 2
+        }
+
+        //sets up invuln pick up
+        if (other.gameObject.tag == "Invulnerablility")
+        {
+            StartCoroutine(IFrames());
+        }
+
+
+
+        // This is a coroutine, it is a timer. It makes the player invincible and invokes Blink repeating for iFramesTime number of seconds
+        IEnumerator IFrames()
+        {
+            // Set isInvincible to true so player can't take damage while coroutine is running
+            isInvincible = true;
+
+            // Sets a timer for iFramesTime number of seconds
+            yield return new WaitForSeconds(iFramesTime);
+
+            // Removes invincibility
+            isInvincible = false;
+
+            // Stops invoking Blink
+            CancelInvoke("Blink");
+
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+    
+
+
 
 
 
