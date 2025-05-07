7f195e2085126dfc94a16372fb5bee66583d6116
OnePunchWeeb <nsumek@gmail.com>
2025-05-06 17:04:31 -0700

    sets build order
    also makes a scene manager, is not working yet

---
+++ Assets/Scripts/Scene Manager Script.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+/*
+ * Nick Sumek
+ * updated 5/6/25
+ * Sets up scene transitions as well as play again and game over function/play again
+ */
+
+public class SceneManagerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
