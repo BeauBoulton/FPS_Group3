d97ea7e1dd75fe6bf1336b4f9465727547c0c8ee
BeauBoulton <beauboulton@gmail.com>
2025-05-08 17:01:39 -0700

    fixed enemy projectile script

--- Assets/Scripts/EnemyProjectile.cs
+++ Assets/Scripts/EnemyProjectile.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class EnemyProjectile : MonoBehaviour
 {
+    public int damage = 1;
+    public float speed = 10;
+    Vector3 spawnPos;
+    public EnemyScript enemyScript;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Sets spawn position at the current position when spawning
+        spawnPos = transform.position;
+
+        damage = enemyScript.GetComponent<EnemyScript>().enemyDamage;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Start moving foraward based on speed set in inspector
+        transform.position += speed * transform.forward * Time.deltaTime;
+
+        // If projectile moves more than 20 units away from spawn position in any direction it despawns
+        if (transform.position.x > (spawnPos.x + 20) || transform.position.x < (spawnPos.x - 20))
+        {
+            Destroy(gameObject);
+        }
+        if (transform.position.y > (spawnPos.y + 20) || transform.position.y < (spawnPos.y - 20))
+        {
+            Destroy(gameObject);
+        }
+        if (transform.position.z > (spawnPos.z + 20) || transform.position.z < (spawnPos.z - 20))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag != "Enemy")
+        {
+            Destroy(gameObject);
+        }
     }
 }
